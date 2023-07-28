ce54798385884cad4380479788f84849a4ae0033
ludo62 <fournier.ludovic@orange.fr>
2023-07-28 11:49:01 +0200

    install mongoose and dotenv

--- src/index.ts
+++ src/index.ts
@@ -16,3 +16,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 
 const server = http.createServer(app);
+
+server.listen(8080, () => {
+    console.log('Le serveur à demarré sur le port 8080');
+})
