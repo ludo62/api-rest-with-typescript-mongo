7da0c41459c127333ce2267f908f49fb6708d1ec
ludo62 <fournier.ludovic@orange.fr>
2023-07-28 13:07:48 +0200

    connect mongoose

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
+require('dotenv').config()
 import express from 'express';
 import http from 'http';
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import compression from 'compression';
 import cors from 'cors';
+import mongoose from 'mongoose';
+
 
 const app = express();
 
@@ -17,6 +20,23 @@ app.use(bodyParser.json());
 
 const server = http.createServer(app);
 
-server.listen(8080, () => {
-    console.log('Le serveur à demarré sur le port 8080');
-})
+const port = process.env.PORT;
+
+// Connexion à la base de données
+const MONGO_URL = process.env.MONGO_URI;
+
+mongoose.Promise = Promise;
+mongoose.connect(MONGO_URL);
+mongoose.connection.on('error', (error: Error) => console.log(error));
+
+const start = async () => {
+    try {
+        server.listen(port, () => {
+            console.log('le serveur à demarré sur le port ' + port);
+        })
+    } catch {
+        console.log('Connexion avec la base de données echouée!');    
+    }
+}
+
+start();
